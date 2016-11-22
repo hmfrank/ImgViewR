09ae5ca774bb7977a88840b80bcb03430d9f455b
Hannes Frank <hannes.frank@online.de>
2016-11-22 22:26:49 +0100

    Added image list to index.php.

--- index.php
+++ index.php
@@ -6,6 +6,25 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+	<h1>ImgViewR Sample Galery</h1>
 
+	<ul>
+<?php
+		$extensions = [ "jpeg", "jpg", "png" ];
+		$files = scandir('.');
+		sort($files);
+
+		foreach ($files as $file)
+		{
+			$ext = pathinfo($file, PATHINFO_EXTENSION);
+			$ext = strtolower($ext);
+
+			if (in_array($ext, $extensions))
+			{
+				echo "\t\t<li>" . $file . "</li>\n";
+			}
+		}
+	?>
+	</ul>
 </body>
 </html>
