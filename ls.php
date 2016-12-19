7f8e4e780fd62de6577ea25fcf812c4fb20c60db
Hannes Frank <hannes.frank@online.de>
2016-12-19 19:57:25 +0100

    Moved file listing code to seperate .php-file.

---
+++ ls.php
@@ -0,0 +1,26 @@
+<?php
+
+$extensions = [ "jpeg", "jpg", "png" ];
+
+// returns true, if the given file is an image file
+function isImage($file)
+{
+	global $extensions;
+
+	$ext = pathinfo($file, PATHINFO_EXTENSION);
+	$ext = strtolower($ext);
+
+	return in_array($ext, $extensions);
+}
+
+// returns a sorted list of all image files in the current working directory
+function getFiles()
+{
+	$list = scandir('.');
+	$list = array_filter($list, "isImage");
+	sort($list);
+
+	return $list;
+}
+
+?>
