bea682c74671ba32970c509a507ce167fb2f75d2
Hannes Frank <hannes.frank@online.de>
2016-12-19 20:02:04 +0100

    Added viewer.php.

---
+++ viewer.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>image viewer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<?php
+	require "ls.php";
+
+	$files = getFiles();
+	
+	$file = $_GET["q"];
+	$index = array_search($file, $files);
+	
+	if ($index === FALSE)
+	{
+		echo "file not found";
+	}
+	else
+	{
+		echo "Index: " . $index;
+	}
+?>
+
+</body>
+</html>
